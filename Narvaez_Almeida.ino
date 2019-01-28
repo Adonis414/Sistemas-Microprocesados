a9bac03e5f611ef90269b17c3ad18250c401e4f8
Adonis414 <44456359+Adonis414@users.noreply.github.com>
2019-01-28 13:36:52 -0500

    Add files via upload

--- Narvaez_Almeida.ino
+++ Narvaez_Almeida.ino
@@ -17,29 +17,50 @@ float32_t var;
 float32_t desvE2;
 float32_t var2;
 
+float32_t REX1[tam_inputSignal];
+float32_t IDX1[tam_inputSignal];
+
+float32_t REX2[tam_sig2];
+float32_t IDX2[tam_sig2];
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
 
+//convolucion
+
   //  arm_conv_f32(&points_ecg_[0], tam_inputSignal, &Impulse_response[0], tam_impulse, &signal_out[0]);
   //  plot_sig(signal_out, tam_inputSignal + tam_impulse);
   //desviacion();
 
-//  suma(&points_ecg_[0], &OutputSignal1[0], tam_inputSignal);
-//  //resta(&InputSignal_1kHz_15kHz[0], &OutputSignal[0], tam_sig);
-//  plot_sig2();
+//suma y resta de la primera señal
+  //suma(&points_ecg_[0], &OutputSignal1[0], tam_inputSignal);
+  //resta(&points_ecg_[0], &OutputSignal1[0], tam_inputSignal);
+  //plot_sig3();
 
-  suma(&Senal[0], &OutputSignal2[0], tam_sig2);
-  //resta(&InputSignal_1kHz_15kHz[0], &OutputSignal[0], tam_sig);
-  plot_sig2();
+//suma y resta de la segunda señal
+  //suma(&Senal[0], &OutputSignal2[0], tam_sig2);
+  //resta(&Senal[0], &OutputSignal2[0], tam_sig2);
+  //plot_sig2();
+
+//transformada de la primera señal
+//  cal_tdf(&points_ecg_[0], &REX1[0], &IDX1[0], tam_inputSignal);
+//  get_tdf1();
+//  plot_signal4();
+
+//transformada de la segunda señal
+  cal_tdf(&Senal[0], &REX2[0], &IDX2[0], tam_sig2);
+  get_tdf2();
+  plot_signal5();
+
 }
 void plot_sig2() {
   uint32_t i;
   for (i = 0; i < tam_sig2; i++) {
-    Serial.println(Senal[i]);
-    //Serial.println(OutputSignal2[i]);
+    //Serial.println(Senal[i]);
+    Serial.println(OutputSignal2[i]);
     delay(10);
   }
 }
@@ -47,51 +68,99 @@ void plot_sig2() {
 void plot_sig3() {
   uint32_t i;
   for (i = 0; i < tam_inputSignal; i++) {
-    
+
     Serial.println(OutputSignal1[i]);
     delay(10);
   }
 }
-  void plot_sig(float32_t *sig_entrada, uint32_t tam_sig) {
-    uint32_t i;
-    for (i = 0; i < tam_sig; i++) {
-      Serial.println(sig_entrada[i] * 100);
-      delay(10);
-    }
+void plot_sig(float32_t *sig_entrada, uint32_t tam_sig) {
+  uint32_t i;
+  for (i = 0; i < tam_sig; i++) {
+    Serial.println(sig_entrada[i] * 100);
+    delay(10);
+  }
+}
+
+void desviacion() {
+
+  //Desviacion de la primera señal de 640 muestras
+  //varianza
+  arm_var_f32(&points_ecg_[0], tam_inputSignal, &var);
+  Serial.println(var);
+  //desviacion estandar
+  desvE = sqrt(var);
+  Serial.println(desvE);
+  delay(1000);
+
+  //Desviacion estandar de la segunda señal de 500 muestras
+  Serial.println(" ");
+  arm_var_f32(&Senal[0], tam_inputSignal, &var2);
+  Serial.println(var2);
+  //desviacion estandar
+  desvE2 = sqrt(var2);
+  Serial.println(desvE2);
+  delay(1000);
+
+}
+
+//suma de las señales
+
+void suma(float32_t *input, float32_t *output, uint32_t tam) {
+  uint32_t i;
+  for (i = 0; i < tam; i++) {
+    output[i] = input[i] + output[i - 1];
+  }
+}
+//resta de las señales
+void resta(float32_t *input, float32_t *output, uint32_t tam) {
+  uint32_t i;
+  for (i = 0; i < tam; i++) {
+    output[i] = input[i] - input[i - 1];
+  }
+}
+
+//calculo de la trasformada discreta de fourier
+void cal_tdf(float32_t *input_sig, float32_t *sig_rex, float32_t *sig_idx, uint32_t tam) {
+
+  uint32_t i, j, k;
+  for (j = 0; j < tam; j++) {
+    sig_rex[j] = 0;
+    sig_idx[j] = 0;
   }
 
-  void desviacion() {
-
-    //Desviacion de la primera señal de 640 muestras
-    //varianza
-    arm_var_f32(&points_ecg_[0], tam_inputSignal, &var);
-    Serial.println(var);
-    //desviacion estandar
-    desvE = sqrt(var);
-    Serial.println(desvE);
-    delay(1000);
-
-    //Desviacion estandar de la segunda señal de 500 muestras
-    Serial.println(" ");
-    arm_var_f32(&Senal[0], tam_inputSignal, &var2);
-    Serial.println(var2);
-    //desviacion estandar
-    desvE2 = sqrt(var2);
-    Serial.println(desvE2);
-    delay(1000);
-
-  }
-
-  void suma(float32_t *input, float32_t *output, uint32_t tam) {
-    uint32_t i;
+  for (k = 0; k < tam; k++) {
     for (i = 0; i < tam; i++) {
-      output[i] = input[i] + output[i - 1];
+      sig_rex[k] = sig_rex[k] + input_sig[i] * cos(2 * PI * k * i / tam);
+      sig_idx[k] = sig_idx[k] - input_sig[i] * sin(2 * PI * k * i / tam);
     }
   }
+}
 
-  void resta(float32_t *input, float32_t *output, uint32_t tam) {
-    uint32_t i;
-    for (i = 0; i < tam; i++) {
-      output[i] = input[i] - input[i - 1];
-    }
+void get_tdf1(void) {
+  uint32_t i;
+  for (i = 0; i < tam_inputSignal; i++) {
+    OutputSignal1[i] = sqrt(pow(REX1[i], 2) + pow(IDX1[i], 2));
   }
+}
+void get_tdf2(void) {
+  uint32_t i;
+  for (i = 0; i < tam_sig2; i++) {
+    OutputSignal2[i] = sqrt(pow(REX2[i], 2) + pow(IDX2[i], 2));
+  }
+}
+//Impresion de la transformada de la primera señal
+void plot_signal4() {
+  uint32_t i;
+  for (i = 0; i < tam_inputSignal; i++) {
+    Serial.println(OutputSignal1[i]);
+
+  }
+}
+
+//Impresion de la transformada de la segunda señal
+void plot_signal5() {
+  uint32_t i;
+  for (i = 0; i < tam_sig2; i++) {
+    Serial.println(OutputSignal2[i]);
+  }
+}
